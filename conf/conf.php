fba451a7231b67b4f19543f02e73a4b1ef9b8754
Exequiels90 <exeschelover@gmail.com>
2026-04-09 13:05:18 -0300

    Update conf.php

--- conf/conf.php
+++ conf/conf.php
@@ -1,10 +1,11 @@
 <?php
-// Archivo de configuracion vacio para bypass de Render
-$dolibarr_main_url_root='https://tu-app-en-render.onrender.com';
+$dolibarr_main_url_root='https://dolibarr-ex3d.onrender.com';
 $dolibarr_main_document_root='/var/www/html/htdocs';
 $dolibarr_main_data_root='/var/www/html/documents';
 $dolibarr_main_db_host='db.pflklctzvmttqdpprota.supabase.co';
 $dolibarr_main_db_port='6543';
 $dolibarr_main_db_name='postgres';
 $dolibarr_main_db_user='postgres';
+$dolibarr_main_db_pass='AUREOX_2022';
 $dolibarr_main_db_type='pgsql';
+$force_install_no_directory_lock=1;
